d2f167ead74ef50e6fc4496789b1c36cfcb4a58c
Diep Le <diep.le@river-island.com>
2019-09-30 11:57:04 +0100

    fixed: lookup() may only be used with flat maps, this map contains elements of type list in

--- terraform-aws-lambda/triggers/s3_notification/main.tf
+++ terraform-aws-lambda/triggers/s3_notification/main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket_notification" "s3_bucket_notification" {
   count  = "${var.enable}"
-  bucket = ["${split(",", lookup(var.s3_config, "bucket"))}"]
+  bucket = ["${split(",", lookup(var.s3_config, "bucket", ""))}"]
 
   lambda_function {
     lambda_function_arn = "${var.lambda_function_arn}"
